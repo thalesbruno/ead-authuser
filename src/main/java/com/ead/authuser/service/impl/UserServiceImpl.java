5ddc3ef47ace641fa7c1c1666b4b4deeda4e7fa2
Thales Bruno <thalesbrno@gmail.com>
2021-10-26 08:01:36 -0300

    feat: first commit

---
+++ src/main/java/com/ead/authuser/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ead.authuser.service.impl;
+
+import com.ead.authuser.repository.UserRepository;
+import com.ead.authuser.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+}
